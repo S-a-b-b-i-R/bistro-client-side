ab4572e60c939834062e5e70ce720643b12ed2f4
S_a_b_b_i_R <sabbirssmc90@gmail.com>
2023-11-25 16:49:18 +0600

    minor chnages

--- src/Components/Loader/Loading.jsx
+++ src/Components/Loader/Loading.jsx
@@ -2,8 +2,10 @@ import Lottie from "lottie-react";
 import animationData from "../../Lottie/loading.json";
 const Loading = () => {
     return (
-        <div className="flex flex-col justify-center items-center min-h-[400px]">
-            <p className="text-4xl">Loading...</p>
+        <div className="flex flex-col min-h-screen justify-center items-center ">
+            <div>
+                <p className="text-4xl">Loading...</p>
+            </div>
             <Lottie animationData={animationData} />
         </div>
     );
